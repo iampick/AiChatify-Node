b9233a7d894a0c70785257eb7016b727d2d1b584
iampick <sir.pick@gmail.com>
2024-11-09 16:23:33 +0700

    fix line openAI

---
+++ routes/api/line-openai-agent/index copy.js
@@ -0,0 +1,276 @@
+const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+const OpenAI = require('openai');
+const openai = new OpenAI();
+const bodyParser = require('body-parser');
+
+const getImageBinary = require('../../../utils/getImageBinary');
+const { uploadFile } = require('../../../utils/cloudinary');
+const waitForStandby = require('./waitForStandby');
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const config = {
+  accessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_SECRET_TOKEN,
+};
+
+const LineHeader = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${config.accessToken}`,
+};
+
+// console.log(client);
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello API from GET' });
+});
+
+router.post(
+  '/',
+  bodyParser.raw({ type: 'application/json' }),
+  async (req, res) => {
+    console.log(req.body);
+
+    if (process.env.LINE_BOT !== 'on') {
+      res.status(200).json({ message: 'Hello API' });
+      return true;
+    }
+
+    const data_raw = req.body;
+    let retrieveMsg = '';
+    let imageParts = '';
+    let files = [];
+    let retrieveImage = '';
+    let userId = '';
+
+    const messageType = data_raw.events[0].message.type;
+    const lineType = data_raw.events[0].type;
+    if (lineType !== 'message') {
+      return true;
+    }
+    if (messageType !== 'text') {
+      return true;
+    }
+    // return res.status(200).json({ message: 'Hello API from GET' });
+
+    const replyToken = data_raw.events[0].replyToken;
+    const sourceType = data_raw.events[0].source.type;
+
+    userId = data_raw.events[0].source.userId;
+    const messageId = data_raw.events[0].message.id;
+
+    if (sourceType === 'group') {
+      userId = data_raw.events[0].source.groupId;
+      lineEndpoint = 'https://api.line.me/v2/bot/message/reply';
+    } else if (sourceType == 'user') {
+      userId = data_raw.events[0].source.userId;
+      lineEndpoint = 'https://api.line.me/v2/bot/message/reply';
+    }
+
+    let conversionId = '';
+    // console.log(messageType);
+    if (messageType === 'text') {
+      retrieveMsg = data_raw.events[0].message.text;
+    } else if (messageType === 'image') {
+      res.status(200).json({ message: 'Hello API' });
+      return true;
+      retrieveImage = await getImageBinary(messageId, LineHeader);
+      // const mimeType = 'image/png';
+      const ImgBuff = Buffer.from(retrieveImage).toString('base64');
+      imageParts = await uploadFile(`data:image/png;base64,${ImgBuff}`, userId);
+      retrieveMsg = 'Please wait for question';
+      console.log(imageParts.url);
+      imageParts = imageParts.url;
+      files = [
+        {
+          type: 'image',
+          transfer_method: 'remote_url',
+          url: imageParts,
+          upload_file_id: '',
+        },
+      ];
+    }
+    // console.log(data_raw.events[0].message);
+    // console.log(files);
+
+    const last10Chars = process.env.OPENAI_ASSISTANT_ID.slice(-10);
+
+    // Query to get all todos from the "todo" table
+    const userInDb = await prisma.UserConv.findFirst({
+      where: {
+        userId: userId,
+        apiId: last10Chars,
+      },
+      orderBy: {
+        id: 'desc',
+      },
+      take: 1,
+    });
+
+    // console.log(userId);
+    // console.log(last10Chars);
+    // console.log(userInDb);
+    if (userInDb) {
+      await waitForStandby(userId, last10Chars);
+
+      conversionId = userInDb.conversionId;
+      const updatedRecord = await prisma.userConv.updateMany({
+        where: {
+          userId: userId,
+          apiId: last10Chars,
+        },
+        data: {
+          status: 'sending',
+        },
+      });
+    }
+
+    let dataToAi = JSON.stringify({
+      message: retrieveMsg,
+      userId: userId,
+      conversionId: conversionId,
+      files: files,
+    });
+
+    // console.log(dataToAi);
+    // connectOpenAi(dataToAi).then(async (response) => {
+    //   console.log(response);
+    // });
+    connectOpenAi(dataToAi)
+      .then(async (response) => {
+        // Assuming `response.data` is a stringified JSON that looks like the given output.
+
+        const rawData = response.message.replace(/\*/g, '');
+        const responseConverId = response.converId;
+
+        const checkUserExist = await prisma.userConv.count({
+          where: {
+            userId: userId,
+            apiId: last10Chars,
+          },
+        });
+
+        if (checkUserExist === 0) {
+          const result = await prisma.userConv.create({
+            data: {
+              userId: userId,
+              conversionId: responseConverId,
+              apiId: last10Chars,
+              status: 'sending',
+            },
+          });
+        }
+
+        const cleanAnswer = rawData.replace(/Final Answer: /g, '');
+        console.log('cleanAnswer');
+        console.log(cleanAnswer);
+
+        const data = {
+          replyToken,
+          messages: [
+            {
+              type: 'text',
+              text: cleanAnswer,
+            },
+          ],
+        };
+        const Lineresponse = await axios.post(
+          'https://api.line.me/v2/bot/message/reply',
+          data,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${config.accessToken}`,
+            },
+          },
+        );
+        const updatedRecord = await prisma.userConv.updateMany({
+          where: {
+            userId: userId,
+            apiId: last10Chars,
+          },
+          data: {
+            status: 'standby',
+          },
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // console.log(JSON.stringify(response.data, null, 4));
+    res.status(200).json({ message: 'Hello API from POST' });
+    // return NextResponse.json({ message: 'Hello API from POST' }, { status: 200 });
+  },
+);
+
+async function connectOpenAi(dataAI) {
+  const api_key = process.env.DIFY_API_KEY; // Ensure you have your API key stored in .env.local
+  const assistant_id = process.env.OPENAI_ASSISTANT_ID; // Ensure you have your API key stored in .env.local
+  const data_raw = JSON.parse(dataAI);
+  let thread_id = '';
+  let compleatAnswer = '';
+
+  if (data_raw.conversionId === '') {
+    const thread = await openai.beta.threads.create();
+    thread_id = thread.id;
+  } else {
+    thread_id = data_raw.conversionId;
+  }
+  console.log(thread_id);
+  const message = await openai.beta.threads.messages.create(thread_id, {
+    role: 'user',
+    content: data_raw.message.trim(),
+  });
+  console.log('message');
+  console.log(message);
+
+  let run = await openai.beta.threads.runs.createAndPoll(thread_id, {
+    assistant_id: assistant_id,
+    instructions: data_raw.message.trim(),
+  });
+
+  if (run.status === 'completed') {
+    const messages = await openai.beta.threads.messages.list(run.thread_id);
+    for (const message of messages.data.reverse()) {
+      if (message.role === 'assistant' && message.content[0].text.value) {
+        console.log('adding assistan response.......');
+        compleatAnswer = message.content[0].text.value;
+      }
+      // console.log(`${message.role} > ${message.content[0].text.value}`);
+    }
+  } else {
+    console.log(run.status);
+  }
+  // console.log(compleatAnswer);
+
+  return { message: compleatAnswer, converId: thread_id };
+}
+
+function logRecursive(obj, depth = 0) {
+  const indent = ' '.repeat(depth * 2); // Indentation for better readability
+
+  if (Array.isArray(obj)) {
+    console.log(indent + '[');
+    obj.forEach((item, index) => {
+      console.log(indent + '  ' + index + ':');
+      logRecursive(item, depth + 1);
+    });
+    console.log(indent + ']');
+  } else if (obj !== null && typeof obj === 'object') {
+    console.log(indent + '{');
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        console.log(indent + '  ' + key + ':');
+        logRecursive(obj[key], depth + 1);
+      }
+    }
+    console.log(indent + '}');
+  } else {
+    console.log(indent + obj);
+  }
+}
+// export the router module so that server.js file can use it
+module.exports = router;
--- routes/api/line-openai-agent/index.js
+++ routes/api/line-openai-agent/index.js
@@ -1,16 +1,15 @@
 const axios = require('axios');
 const express = require('express');
-const router = express.Router();
-const OpenAI = require('openai');
-const openai = new OpenAI();
 const bodyParser = require('body-parser');
-
+const { PrismaClient } = require('@prisma/client');
+const OpenAI = require('openai');
 const getImageBinary = require('../../../utils/getImageBinary');
 const { uploadFile } = require('../../../utils/cloudinary');
 const waitForStandby = require('./waitForStandby');
 
-const { PrismaClient } = require('@prisma/client');
+const router = express.Router();
 const prisma = new PrismaClient();
+const openai = new OpenAI();
 
 const config = {
   accessToken: process.env.LINE_ACCESS_TOKEN,
@@ -22,235 +21,148 @@ const LineHeader = {
   Authorization: `Bearer ${config.accessToken}`,
 };
 
-// console.log(client);
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello API from GET' });
 });
 
-router.post(
-  '/',
-  bodyParser.raw({ type: 'application/json' }),
-  async (req, res) => {
-    console.log(req.body);
+router.post('/', bodyParser.raw({ type: 'application/json' }), async (req, res) => {
+  console.log(req.body);
 
-    if (process.env.LINE_BOT !== 'on') {
-      res.status(200).json({ message: 'Hello API' });
-      return true;
-    }
+  if (process.env.LINE_BOT !== 'on') {
+    return res.status(200).json({ message: 'Hello API' });
+  }
 
-    const data_raw = req.body;
-    let retrieveMsg = '';
-    let imageParts = '';
-    let files = [];
-    let retrieveImage = '';
-    let userId = '';
+  const data_raw = req.body;
+  const { messageType, lineType, replyToken, sourceType, userId, messageId } = extractEventData(data_raw);
 
-    const messageType = data_raw.events[0].message.type;
-    const lineType = data_raw.events[0].type;
-    if (lineType !== 'message') {
-      return true;
-    }
-    if (messageType !== 'text') {
-      return true;
-    }
-    // return res.status(200).json({ message: 'Hello API from GET' });
+  if (lineType !== 'message' || messageType !== 'text') {
+    return res.status(200).json({ message: 'Hello API' });
+  }
 
-    const replyToken = data_raw.events[0].replyToken;
-    const sourceType = data_raw.events[0].source.type;
+  const lineEndpoint = getLineEndpoint(sourceType, userId);
+  const retrieveMsg = await handleMessageType(messageType, messageId, userId);
 
-    userId = data_raw.events[0].source.userId;
-    const messageId = data_raw.events[0].message.id;
+  const conversionId = await getConversionId(userId);
+  const dataToAi = JSON.stringify({ message: retrieveMsg, userId, conversionId, files: [] });
 
-    if (sourceType === 'group') {
-      userId = data_raw.events[0].source.groupId;
-      lineEndpoint = 'https://api.line.me/v2/bot/message/reply';
-    } else if (sourceType == 'user') {
-      userId = data_raw.events[0].source.userId;
-      lineEndpoint = 'https://api.line.me/v2/bot/message/reply';
-    }
-
-    let conversionId = '';
-    // console.log(messageType);
-    if (messageType === 'text') {
-      retrieveMsg = data_raw.events[0].message.text;
-    } else if (messageType === 'image') {
-      res.status(200).json({ message: 'Hello API' });
-      return true;
-      retrieveImage = await getImageBinary(messageId, LineHeader);
-      // const mimeType = 'image/png';
-      const ImgBuff = Buffer.from(retrieveImage).toString('base64');
-      imageParts = await uploadFile(`data:image/png;base64,${ImgBuff}`, userId);
-      retrieveMsg = 'Please wait for question';
-      console.log(imageParts.url);
-      imageParts = imageParts.url;
-      files = [
-        {
-          type: 'image',
-          transfer_method: 'remote_url',
-          url: imageParts,
-          upload_file_id: '',
-        },
-      ];
-    }
-    // console.log(data_raw.events[0].message);
-    // console.log(files);
-
-    const last10Chars = process.env.OPENAI_ASSISTANT_ID.slice(-10);
-
-    // Query to get all todos from the "todo" table
-    const userInDb = await prisma.UserConv.findFirst({
-      where: {
-        userId: userId,
-        apiId: last10Chars,
-      },
-      orderBy: {
-        id: 'desc',
-      },
-      take: 1,
-    });
-
-    // console.log(userId);
-    // console.log(last10Chars);
-    // console.log(userInDb);
-    if (userInDb) {
-      await waitForStandby(userId, last10Chars);
-
-      conversionId = userInDb.conversionId;
-      const updatedRecord = await prisma.userConv.updateMany({
-        where: {
-          userId: userId,
-          apiId: last10Chars,
-        },
-        data: {
-          status: 'sending',
-        },
-      });
-    }
-
-    let dataToAi = JSON.stringify({
-      message: retrieveMsg,
-      userId: userId,
-      conversionId: conversionId,
-      files: files,
-    });
-
-    // console.log(dataToAi);
-    // connectOpenAi(dataToAi).then(async (response) => {
-    //   console.log(response);
-    // });
-    connectOpenAi(dataToAi)
-      .then(async (response) => {
-        // Assuming `response.data` is a stringified JSON that looks like the given output.
-
-        const rawData = response.message.replace(/\*/g, '');
-        const responseConverId = response.converId;
-
-        const checkUserExist = await prisma.userConv.count({
-          where: {
-            userId: userId,
-            apiId: last10Chars,
-          },
-        });
-
-        if (checkUserExist === 0) {
-          const result = await prisma.userConv.create({
-            data: {
-              userId: userId,
-              conversionId: responseConverId,
-              apiId: last10Chars,
-              status: 'sending',
-            },
-          });
-        }
-
-        const cleanAnswer = rawData.replace(/Final Answer: /g, '');
-        console.log('cleanAnswer');
-        console.log(cleanAnswer);
-
-        const data = {
-          replyToken,
-          messages: [
-            {
-              type: 'text',
-              text: cleanAnswer,
-            },
-          ],
-        };
-        const Lineresponse = await axios.post(
-          'https://api.line.me/v2/bot/message/reply',
-          data,
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${config.accessToken}`,
-            },
-          },
-        );
-        const updatedRecord = await prisma.userConv.updateMany({
-          where: {
-            userId: userId,
-            apiId: last10Chars,
-          },
-          data: {
-            status: 'standby',
-          },
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    // console.log(JSON.stringify(response.data, null, 4));
+  try {
+    const response = await connectOpenAi(dataToAi);
+    await handleOpenAiResponse(response, userId, replyToken);
     res.status(200).json({ message: 'Hello API from POST' });
-    // return NextResponse.json({ message: 'Hello API from POST' }, { status: 200 });
-  },
-);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+function extractEventData(data_raw) {
+  const event = data_raw.events[0];
+  return {
+    messageType: event.message.type,
+    lineType: event.type,
+    replyToken: event.replyToken,
+    sourceType: event.source.type,
+    userId: event.source.userId,
+    messageId: event.message.id,
+  };
+}
+
+function getLineEndpoint(sourceType, userId) {
+  if (sourceType === 'group') {
+    return 'https://api.line.me/v2/bot/message/reply';
+  } else if (sourceType === 'user') {
+    return 'https://api.line.me/v2/bot/message/reply';
+  }
+}
+
+async function handleMessageType(messageType, messageId, userId) {
+  if (messageType === 'text') {
+    return data_raw.events[0].message.text;
+  } else if (messageType === 'image') {
+    const retrieveImage = await getImageBinary(messageId, LineHeader);
+    const ImgBuff = Buffer.from(retrieveImage).toString('base64');
+    const imageParts = await uploadFile(`data:image/png;base64,${ImgBuff}`, userId);
+    return 'Please wait for question';
+  }
+}
+
+async function getConversionId(userId) {
+  const last10Chars = process.env.OPENAI_ASSISTANT_ID.slice(-10);
+  const userInDb = await prisma.UserConv.findFirst({
+    where: { userId, apiId: last10Chars },
+    orderBy: { id: 'desc' },
+    take: 1,
+  });
+
+  if (userInDb) {
+    await waitForStandby(userId, last10Chars);
+    await prisma.userConv.updateMany({
+      where: { userId, apiId: last10Chars },
+      data: { status: 'sending' },
+    });
+    return userInDb.conversionId;
+  }
+  return '';
+}
+
+async function handleOpenAiResponse(response, userId, replyToken) {
+  const rawData = response.message.replace(/\*/g, '');
+  const responseConverId = response.converId;
+  const last10Chars = process.env.OPENAI_ASSISTANT_ID.slice(-10);
+
+  const checkUserExist = await prisma.userConv.count({
+    where: { userId, apiId: last10Chars },
+  });
+
+  if (checkUserExist === 0) {
+    await prisma.userConv.create({
+      data: { userId, conversionId: responseConverId, apiId: last10Chars, status: 'sending' },
+    });
+  }
+
+  const cleanAnswer = rawData.replace(/Final Answer: /g, '');
+  const data = {
+    replyToken,
+    messages: [{ type: 'text', text: cleanAnswer }],
+  };
+
+  await axios.post('https://api.line.me/v2/bot/message/reply', data, { headers: LineHeader });
+  await prisma.userConv.updateMany({
+    where: { userId, apiId: last10Chars },
+    data: { status: 'standby' },
+  });
+}
 
 async function connectOpenAi(dataAI) {
-  const api_key = process.env.DIFY_API_KEY; // Ensure you have your API key stored in .env.local
-  const assistant_id = process.env.OPENAI_ASSISTANT_ID; // Ensure you have your API key stored in .env.local
+  const api_key = process.env.DIFY_API_KEY;
+  const assistant_id = process.env.OPENAI_ASSISTANT_ID;
   const data_raw = JSON.parse(dataAI);
-  let thread_id = '';
-  let compleatAnswer = '';
+  let thread_id = data_raw.conversionId || (await openai.beta.threads.create()).id;
 
-  if (data_raw.conversionId === '') {
-    const thread = await openai.beta.threads.create();
-    thread_id = thread.id;
-  } else {
-    thread_id = data_raw.conversionId;
-  }
-  console.log(thread_id);
-  const message = await openai.beta.threads.messages.create(thread_id, {
+  await openai.beta.threads.messages.create(thread_id, {
     role: 'user',
     content: data_raw.message.trim(),
   });
-  console.log('message');
-  console.log(message);
 
-  let run = await openai.beta.threads.runs.createAndPoll(thread_id, {
-    assistant_id: assistant_id,
+  const run = await openai.beta.threads.runs.createAndPoll(thread_id, {
+    assistant_id,
     instructions: data_raw.message.trim(),
   });
 
   if (run.status === 'completed') {
     const messages = await openai.beta.threads.messages.list(run.thread_id);
-    for (const message of messages.data.reverse()) {
-      if (message.role === 'assistant' && message.content[0].text.value) {
-        console.log('adding assistan response.......');
-        compleatAnswer = message.content[0].text.value;
-      }
-      // console.log(`${message.role} > ${message.content[0].text.value}`);
-    }
-  } else {
-    console.log(run.status);
-  }
-  // console.log(compleatAnswer);
+    const compleatAnswer = messages.data.reverse().find(
+      (msg) => msg.role === 'assistant' && msg.content[0].text.value
+    ).content[0].text.value;
 
-  return { message: compleatAnswer, converId: thread_id };
+    return { message: compleatAnswer, converId: thread_id };
+  } else {
+    throw new Error(run.status);
+  }
 }
 
 function logRecursive(obj, depth = 0) {
-  const indent = ' '.repeat(depth * 2); // Indentation for better readability
+  const indent = ' '.repeat(depth * 2);
 
   if (Array.isArray(obj)) {
     console.log(indent + '[');
@@ -272,5 +184,5 @@ function logRecursive(obj, depth = 0) {
     console.log(indent + obj);
   }
 }
-// export the router module so that server.js file can use it
+
 module.exports = router;
